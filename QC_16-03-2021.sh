9ed05482c1a419dffd03bd03eac797533a8fe56f
Ben <59935248+BenNicolet@users.noreply.github.com>
2021-03-16 14:31:06 +0100

    Update QC_16-03-2021.sh

--- QC_16-03-2021.sh
+++ QC_16-03-2021.sh
@@ -2,6 +2,11 @@
 ### b.nicolet@sanquin.nl
 
 
-cd /home/ben/Analysis/TCR_analysis/raw_fastq/Rosa/
+###### This scripts will run fastqc on all files in the indicated folder ######
 
-fastqc -t 22 -f fastq *.fastq -o /home/ben/Analysis/TCR_analysis/raw_fastq/Rosa/QC/
+# change to the directory where the raw fastq files are :
+cd /path-to-files/
+
+
+## calling fastqc, with 22 threads ("processors"), -f is file type (here fastq), -o is for output directory.
+fastqc -t 22 -f fastq *.fastq -o /path-to-output/QC/
